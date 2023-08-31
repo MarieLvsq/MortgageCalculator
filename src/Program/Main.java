e057f505faba2328adf649f6f95667fcac8743fe
MarieLvsq <maarielevesque@gmail.com>
2023-08-31 22:53:55 +0200

    correction : ajout constantes + noms variables de résultat

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -7,17 +7,19 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		byte MONTHS_IN_YEAR = 12;
+		byte PERCENT = 100;
 		System.out.print("Principal:");
 		Scanner scanner = new Scanner(System.in);
 		int principal = scanner.nextInt();
 		System.out.print("Annual Interest:");
-		double monthlyInterest = scanner.nextDouble()/1200;
+		double monthlyInterest = scanner.nextDouble()/MONTHS_IN_YEAR/PERCENT;
 		System.out.print("Period (Years):");
-		int period = scanner.nextInt() *12;
+		int period = scanner.nextInt() *MONTHS_IN_YEAR;
 		double interestRatePow = Math.pow((1+monthlyInterest), period);
 		double resultDouble = principal * ((monthlyInterest*interestRatePow)/(interestRatePow-1));
-		String result = NumberFormat.getCurrencyInstance().format(resultDouble);
-		System.out.println("Mortgage:" + result);
+		String mortgage = NumberFormat.getCurrencyInstance().format(resultDouble);
+		System.out.println("Mortgage:" + mortgage);
 	}
 
 }
